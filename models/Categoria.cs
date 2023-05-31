66e724e410b5483aef59f5c4e665d0fdb7385d25
Fabo <fabian.de193@tectijuana.edu.mx>
2023-05-31 03:15:05 -0700

    get, post, put and delete endpoint

--- models/Categoria.cs
+++ models/Categoria.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace proyectoef.Models;
 
@@ -12,6 +13,6 @@ public class Categoria {
     public string Descripcion { get; set; }
 
     public int peso { get; set; }
-
+    [JsonIgnore]
     public virtual ICollection<Tarea> Tareas { get; set; } //1 categoria tiene muchas tareas
 }
--- program.cs
+++ program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using proyectoef;
+using proyectoef.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +16,59 @@ app.MapGet("/dbconexion", async ([FromServices] TareasContext dbContext) =>
 {
     dbContext.Database.EnsureCreated();
     return Results.Ok("Base de datos creada:" + dbContext.Database.IsInMemory());
+});
+
+app.MapGet("/api/tareas", async ([FromServices] TareasContext dbContext) =>
+{
+    return Results.Ok(dbContext.Tareas.Include(p=>p.Categoria).Where(p => p.PrioridadTarea == proyectoef.Models.Prioridad.baja).ToList()); //Obtenemos las tareas con prioridad baja
+});
+
+app.MapPost("/api/tareas", async ([FromServices] TareasContext dbContext, [FromBody] Tarea tarea) =>
+{
+    tarea.TareaID = Guid.NewGuid();
+    tarea.FechaCreacion = DateTime.Now;
+    await dbContext.AddAsync(tarea);
+    //await dbContext.Tareas.AddAsync(tarea);
+
+    await dbContext.SaveChangesAsync();
+    return Results.Ok(); 
+});
+
+app.MapPut("/api/tareas/{id}", async ([FromServices] TareasContext dbContext, [FromBody] Tarea tarea, [FromRoute] Guid id) =>
+{
+    var tareaActual = dbContext.Tareas.Find(id);
+
+    if(tareaActual != null)
+    {
+        tareaActual.CategoriaID  = tarea.CategoriaID;
+        tareaActual.Nombre = tarea.Nombre;
+        tareaActual.Descripcion = tarea.Descripcion;
+        tareaActual.PrioridadTarea = tarea.PrioridadTarea;
+
+        await dbContext.SaveChangesAsync();
+        return Results.Ok();
+    }
+    else
+    {
+        return Results.NotFound();
+    }
+});
+
+app.MapDelete("/api/tareas/{id}", async ([FromServices] TareasContext dbContext, [FromRoute] Guid id) =>
+{
+    var tareaActual = dbContext.Tareas.Find(id);
+
+    if(tareaActual != null)
+    {
+        dbContext.Remove(tareaActual);
+        await dbContext.SaveChangesAsync();
+
+        return Results.Ok();
+    }
+    else
+    {
+        return Results.NotFound();
+    }
 });
 
 app.Run();
